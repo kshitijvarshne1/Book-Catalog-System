1ed714176deea546cea05248c391dc3f766e3ee0
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-25 17:24:20 +0530

    Initial commit

---
+++ src/test/java/com/example/bookcatalogsystem/BookCatalogSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookcatalogsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookCatalogSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

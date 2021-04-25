86cc6f0979383c381fa94636d9df698a682c5b39
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-25 17:30:25 +0530

    create BookController.java

---
+++ src/main/java/com/example/bookcatalogsystem/Controller/BookController.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 25-Apr-21
+ *   Time: 5:30 PM
+ *   File: BookController.java
+ */
+
+package com.example.bookcatalogsystem.Controller;
+
+public class BookController {
+}
+

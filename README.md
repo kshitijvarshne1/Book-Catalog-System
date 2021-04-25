ed2da82ea0616557952c523f13120888db3cfa97
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-25 19:22:13 +0530

    final commit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Book-Catalog-System
 Using SpringBoot and RestAPI
+
+# Following RestAPIs
+
+#  
+
+![img.png](img.png)
+#
+![img_1.png](img_1.png)
+#
+![img_2.png](img_2.png)
+#
+![img_3.png](img_3.png)
+#
+![img_4.png](img_4.png)
--- src/main/java/com/example/bookcatalogsystem/BookCatalogSystemApplication.java
+++ src/main/java/com/example/bookcatalogsystem/BookCatalogSystemApplication.java
@@ -2,8 +2,10 @@ package com.example.bookcatalogsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class BookCatalogSystemApplication {
 
     public static void main(String[] args) {
